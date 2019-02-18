e2eb0e414037438df136d462f023705c81e2205b
Graham Swan <thinkswan@gmail.com>
2019-02-18 21:26:51 +0100

    Add server config

---
+++ config/server.js
@@ -0,0 +1,7 @@
+module.exports = {
+  ACCESS_KEY: process.env.ACCESS_KEY ||
+    'a45d8d5153b51bd885114b73a6719941a9b9f3c369b63d3aafb3bba59364abd5',
+  SECRET_KEY: process.env.ACCESS_KEY ||
+    '30ae62f6b86aaaa50fcc998541e8f9f7a3215dbf5220598c1c4e0bdc0892ef56',
+  CALLBACK_URL: process.env.CALLBACK_URL || 'http://localhost:3000'
+}
